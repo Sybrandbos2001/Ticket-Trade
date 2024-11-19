e7b7f593cb75e10e7c22c3d0c32d4809ae1ad486
Sybrand Bos <snjw.bos@student.avans.nl>
2024-11-19 22:59:43 +0100

    E2E testing for API

--- apps/ticket-trade-api-e2e/src/ticket-trade-api/ticket-trade-api.spec.ts
+++ apps/ticket-trade-api-e2e/src/ticket-trade-api/ticket-trade-api.spec.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const baseURL = process.env.API_URL || 'http://localhost:3000';
+
 describe('GET /api', () => {
   it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+    const res = await axios.get(`${baseURL}/api`);
 
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello API' });
